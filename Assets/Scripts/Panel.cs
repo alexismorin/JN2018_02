1042af2a13194cc14999156177a1639ce7c0745d
Alexis <alexis.morin.mail@gmail.com>
2018-11-24 10:52:07 -0500

    blocking room

---
+++ Assets/Scripts/Panel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour {
+
+    public GameObject panelObject;
+
+    public void DisplayItem (Texture newTexture) {
+        panelObject.GetComponent<MeshRenderer> ().material.SetTexture ("_MainTex", newTexture);
+        GetComponent<Animator> ().SetTrigger ("Open");
+    }
+
+}
